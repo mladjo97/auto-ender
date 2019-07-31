6f7259aea41580921e2f074ada84d0d88342c1c2
Mladen Milosevic <mldnmilosevic@gmail.com>
2019-07-31 13:08:55 +0200

    added initial mod behaviours

---
+++ AutoEnder/AutoEnder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace AutoEnder
+{
+    public class AutoEnder : ModMeta
+    {
+        public override string Name => "Design and Development Auto-Ender mod";
+
+        public override void ConstructOptionsScreen(RectTransform parent, bool inGame)
+        {
+            return;
+        }
+    }
+}
---
+++ AutoEnder/AutoEnderBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoEnder
+{
+    public class AutoEnderBehaviour : ModBehaviour
+    {
+        private bool _isActive = false;
+
+        public bool IsActive
+        {
+            get { return _isActive; }
+            set { _isActive = value; }
+        }
+
+        public override void OnActivate()
+        {
+            _isActive = true;
+        }
+
+        public override void OnDeactivate()
+        {
+            _isActive = false;
+        }
+
+        public void Update()
+        {
+            if (_isActive
+                && GameSettings.Instance != null
+                && GameSettings.Instance.MyCompany != null
+                && GameSettings.Instance.MyCompany.WorkItems != null)
+            {
+                /*
+                 * Continue when design phase is done
+                 */
+                IEnumerable<DesignDocument> designPhase = GameSettings.Instance.MyCompany.WorkItems.OfType<DesignDocument>();
+                foreach (DesignDocument document in designPhase)
+                {
+                    if (document.HasFinished && !document.Done)
+                    {
+                        document.PromoteAction();
+                    }
+                }
+
+                /*
+                 * Continue when alpha phase is done
+                 */
+                IEnumerable<SoftwareAlpha> alphaPhase = GameSettings.Instance.MyCompany.WorkItems.OfType<SoftwareAlpha>();
+
+                foreach (SoftwareAlpha alpha in alphaPhase)
+                {
+                    if (alpha.HasFinished && !alpha.Done)
+                    {
+                        alpha.PromoteAction();
+                    }
+                }
+            }
+
+        }
+
+    }
+}
